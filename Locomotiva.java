ebc094c46c0f9adff3f15f8ad6f64ce207d62e31
Paulo-Mansano <145067044+Paulo-Mansano@users.noreply.github.com>
2023-10-17 12:37:57 -0300

    Update Locomotiva.java

--- Locomotiva.java
+++ Locomotiva.java
@@ -1,112 +1,168 @@
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Classe que representa uma locomotiva.
+ */
 public class Locomotiva {
 
-    //Variáveis e encapsulamento:
-    private int idLoc;
-    private double pesoMax; // Unidade de medida: toneladas.
-    private int qntVagoes;
-    private Composicao composicao;
+    // Variáveis e encapsulamento:
 
-    private boolean livre = true; //
+    private int idLoc; // Identificador único da locomotiva.
+    private double pesoMax; // Peso máximo suportado pela locomotiva (em toneladas).
+    private int qntVagoes; // Quantidade de vagões que a locomotiva pode puxar.
+    private Composicao composicao; // Composição à qual a locomotiva está vinculada.
 
-    //Construtor:
+    // Construtores:
 
+    /**
+     * Cria uma nova instância de locomotiva com composição.
+     *
+     * @param idLoc      O identificador único da locomotiva.
+     * @param pesoMax    O peso máximo suportado pela locomotiva (em toneladas).
+     * @param qntVagoes  A quantidade de vagões que a locomotiva pode puxar.
+     * @param composicao A composição à qual a locomotiva está vinculada.
+     */
     public Locomotiva(int idLoc, double pesoMax, int qntVagoes, Composicao composicao) {
         this.idLoc = idLoc;
         this.pesoMax = pesoMax;
         this.qntVagoes = qntVagoes;
-        this.composicao = null;
-
-        if (this.composicao != null) {
-            this.livre = false;
-        } else {
-            this.livre = false;
-        }
-    }
-    
-    //Métodos:
-    
-    public void engatarEmComposicao(Composicao composicao) {
-        if (this.composicao == null) {
-            this.composicao = composicao;
-            composicao.adicionarLocomotiva(this);
-        } else {
-            System.out.println("Esta locomotiva já está engatada em uma composição.");
-        }
-    }
-
-    public void desengatar() {
-        if (this.composicao != null) {
-            this.composicao.removerLocomotiva(this);
-            this.composicao = null;
-        } else {
-            System.out.println("Esta locomotiva não está engatada em nenhuma composição.");
-        }
-    }
-
-    public String getDisponibilidade() {
-        final String dispLivre = "Livre";
-        final String dispOcupado = "Ocupado";
-
-        if (this.livre == true) {
-            return dispLivre;           
-        } else {
-            return dispOcupado;
-        }
-        
+        this.composicao = composicao;
     }
 
     /**
-     * Getters e Setters - Organização:
-     * 
-     * getMetodoX() {};
-     * setMetodoX(type idVar) {};
-     * 
-     * getMetodoY() {};
-     * setMetodoY(type idVar) {};
-     * 
+     * Cria uma nova instância de locomotiva sem composição.
+     *
+     * @param idLoc     O identificador único da locomotiva.
+     * @param pesoMax   O peso máximo suportado pela locomotiva (em toneladas).
+     * @param qntVagoes A quantidade de vagões que a locomotiva pode puxar.
      */
+    public Locomotiva(int idLoc, double pesoMax, int qntVagoes) {
+        this.idLoc = idLoc;
+        this.pesoMax = pesoMax;
+        this.qntVagoes = qntVagoes;
+        this.composicao = null;
+    }
 
+    // Métodos:
+
+    /**
+     * Engata a locomotiva em uma composição.
+     *
+     * @param composicao A composição à qual a locomotiva será engatada.
+     * @throws RuntimeException Se a locomotiva já estiver engatada em uma
+     *                          composição.
+     */
+    public void engatarEmComposicao(Composicao composicao) {
+        if (this.composicao != null) {
+            throw new RuntimeException("Esta locomotiva já está engatada em uma composição.");
+        }
+        this.composicao = composicao;
+        composicao.adicionarLocomotiva(this);
+    }
+
+    /**
+     * Desengata a locomotiva da composição à qual está vinculada.
+     *
+     * @throws RuntimeException Se a locomotiva não estiver engatada em nenhuma
+     *                          composição.
+     */
+    public void desengatar() {
+        if (this.composicao == null) {
+            throw new RuntimeException("Esta locomotiva não está engatada em nenhuma composição.");
+        }
+        this.composicao.removerLocomotiva(this);
+        this.composicao = null;
+    }
+
+    /**
+     * Obtém a disponibilidade da locomotiva (livre ou ocupada).
+     *
+     * @return "Livre" se a locomotiva não estiver em uma composição, "Ocupada" caso
+     *         contrário.
+     */
+    public String getDisponibilidade() {
+        return (composicao == null) ? "Livre" : "Ocupado";
+    }
+
+    // Getters e Setters - Organização:
+
+    /**
+     * Obtém o identificador único da locomotiva.
+     *
+     * @return O identificador da locomotiva.
+     */
     public int getIdLoc() {
         return idLoc;
     }
 
+    /**
+     * Define o identificador único da locomotiva.
+     *
+     * @param idLoc O identificador da locomotiva.
+     */
     public void setIdLoc(int idLoc) {
         this.idLoc = idLoc;
     }
 
+    /**
+     * Obtém o peso máximo suportado pela locomotiva.
+     *
+     * @return O peso máximo da locomotiva em toneladas.
+     */
     public double getPesoMax() {
         return pesoMax;
     }
 
-    public void setPesoMaximo(double pesoMax) {
+    /**
+     * Define o peso máximo suportado pela locomotiva.
+     *
+     * @param pesoMax O peso máximo da locomotiva em toneladas.
+     */
+    public void setPesoMax(double pesoMax) {
         this.pesoMax = pesoMax;
     }
 
+    /**
+     * Obtém a quantidade de vagões que a locomotiva pode puxar.
+     *
+     * @return A quantidade de vagões.
+     */
     public int getQntVagoes() {
         return qntVagoes;
     }
 
+    /**
+     * Define a quantidade de vagões que a locomotiva pode puxar.
+     *
+     * @param qntVagoes A quantidade de vagões.
+     */
     public void setQntVagoes(int qntVagoes) {
         this.qntVagoes = qntVagoes;
     }
 
+    /**
+     * Obtém a composição à qual a locomotiva está vinculada.
+     *
+     * @return A composição da locomotiva ou null se não estiver vinculada a nenhuma
+     *         composição.
+     */
     public Composicao getComposicao() {
         return composicao;
     }
 
+    /**
+     * Define a composição à qual a locomotiva está vinculada.
+     *
+     * @param composicao A composição da locomotiva.
+     */
     public void setComposicao(Composicao composicao) {
         this.composicao = composicao;
     }
 
     /**
-     * Método toString() da Classe:
+     * Retorna uma representação em formato de string da locomotiva.
+     *
+     * @return Uma string que descreve a locomotiva.
      */
-
     @Override
-
     public String toString() {
         return "idLoc: " + getIdLoc() + "\n" +
                 "pesoMax: " + getPesoMax() + "\n" +
@@ -114,6 +170,5 @@ public class Locomotiva {
                 "Composicao: " + getComposicao() + "\n" +
                 "pesoMax: " + getPesoMax() + "\n" +
                 "Livre: " + getDisponibilidade() + "\n";
-
     }
 }
